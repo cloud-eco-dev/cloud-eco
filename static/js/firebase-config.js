548eeed4d2e445b83f1f0f6b15932cd5500e13dd
cloud-net <crp203426@gmail.com>
2026-02-24 15:38:52 +0500

    Add files via upload

---
+++ static/js/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCp5Rg1nDLElTwsKVI71tmduhdQIgGJ_NQ",
+  authDomain: "cloudnet-io.firebaseapp.com",
+  projectId: "cloudnet-io",
+  storageBucket: "cloudnet-io.firebasestorage.app",
+  messagingSenderId: "487910450412",
+  appId: "1:487910450412:web:92731cd7a26bf0ac38b1c9"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
